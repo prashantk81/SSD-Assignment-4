f8e2012be508453a29b112c24bd62f49268b38a7
Prashant Kumar <prashantkumarr81@gmail.com>
2022-10-25 17:21:09 +0530

    pattern question

---
+++ pattern.py
@@ -0,0 +1,19 @@
+def upperPattern():
+    print("           ____________")
+    print("          /            \\")
+    print("         /              \\")
+   
+def lowerPattern():
+    print("         \\              /")    
+    print("          \\____________/")   
+
+upperPattern()    
+# print("\n")
+print("          +------------+")
+upperPattern()
+lowerPattern()
+lowerPattern()
+print("          +------------+")
+upperPattern()
+print("         |     STOP     |")
+lowerPattern()
